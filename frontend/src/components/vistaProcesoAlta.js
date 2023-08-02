86e43a7f0f936cc00fd424ef8c8b24c695488327
-AcademiaPostal- <bujias@proton.me>
2023-08-02 10:01:41 +0200

    El token se muestra en pantalla

--- frontend/src/components/vistaProcesoAlta.js
+++ frontend/src/components/vistaProcesoAlta.js
@@ -1,19 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const VistaProcesoAlta = () => {
+  const [message, setMessage] = useState('');
+
   useEffect(() => {
     // Extract URL parameter "token" from the URL
     const urlParams = new URLSearchParams(window.location.search);
     const token = urlParams.get('token');
 
     if (token) {
-      console.log(`URL de validación: ${window.location.origin}/validate/${token}`);
+      const validationURL = `${window.location.origin}/validate/${token}`;
+      setMessage(`URL de validación: ${validationURL}`);
     }
   }, []);
 
   return (
     <div>
       <h1>El componente funciona</h1>
+      {message && <p>{message}</p>}
     </div>
   );
 };
